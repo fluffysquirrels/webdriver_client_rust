a189b1b67955ae074839997652fdcdb967defceb
Rui Abreu Ferreira <raf-ep@gmx.com>
2017-06-01 08:37:47 +0100

    Listen on random TCP port geckdriver
    Instead of a hard coded default TCP port for geckodriver, use an
    ephemeral TCP port.

--- src/firefox.rs
+++ src/firefox.rs
@@ -3,9 +3,22 @@ use super::*;
 use std::process::{Command, Child, Stdio};
 use std::thread;
 use std::time::Duration;
+use std::net::TcpListener;
+
+
+/// Find a TCP port number to use. This is racy but see
+/// https://bugzilla.mozilla.org/show_bug.cgi?id=1240830
+///
+/// If port is Some, check if we can bind to the given port. Otherwise
+/// pick a random port.
+fn check_tcp_port(port: Option<u16>) -> io::Result<u16> {
+    TcpListener::bind(&("localhost", port.unwrap_or(0)))
+        .and_then(|stream| stream.local_addr())
+        .map(|x| x.port())
+}
 
 pub struct GeckoDriverBuilder {
-    port: u16,
+    port: Option<u16>,
     ff_binary: String,
     kill_on_drop: bool,
 }
@@ -13,7 +26,7 @@ pub struct GeckoDriverBuilder {
 impl GeckoDriverBuilder {
     pub fn new() -> Self {
         GeckoDriverBuilder {
-            port: 4444,
+            port: None,
             ff_binary: "firefox".to_owned(),
             kill_on_drop: true,
         }
@@ -31,11 +44,13 @@ impl GeckoDriverBuilder {
         self
     }
     pub fn spawn(self) -> Result<GeckoDriver, Error> {
+        let port = check_tcp_port(self.port)?;
+
         let child = Command::new("geckodriver")
             .arg("-b")
             .arg(self.ff_binary)
             .arg("--webdriver-port")
-            .arg(format!("{}", self.port))
+            .arg(format!("{}", port))
             .stdin(Stdio::null())
             .stderr(Stdio::null())
             .stdout(Stdio::null())
@@ -45,7 +60,7 @@ impl GeckoDriverBuilder {
         thread::sleep(Duration::new(1, 500));
         Ok(GeckoDriver {
             child: child,
-            url: format!("http://localhost:{}", self.port),
+            url: format!("http://localhost:{}", port),
             kill_on_drop: self.kill_on_drop,
         })
     }
